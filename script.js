ea09bb8ba01fafdec6beb9547d7036c998eb350b
Olly <ollycodesdev@gmail.com>
2026-04-11 18:05:31 +0200

    Add getUserChoice function
    This functions gets the user input and returns
    rock paper or scissors

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function getComputerChoice() {
     let options = ['rock', 'paper', 'scissors']
     return options[Math.floor(Math.random() * options.length)]
 }
+
+function getUserChoice() {
+    let choice = prompt("Rock, Paper or Scissors?").toLowerCase()
+    return choice
+}
