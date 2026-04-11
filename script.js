ad7bb2b3b4a15c85c4546ee922c37a027d68bc0f
Olly <ollycodesdev@gmail.com>
2026-04-11 20:50:46 +0200

    Add playRound function
    Allows the user to play a round. It displays the
    winner and increments the score for the winner

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ function getUserChoice() {
     let choice = prompt("Rock, Paper or Scissors?").toLowerCase()
     return choice
 }
+
+let userScore = 0;
+let computerScore = 0;
+
+function playRound(computerChoice, userChoice) {
+    if (userChoice === computerChoice) {
+        console.log("tie");
+    } else if (userChoice === 'rock' && computerChoice === 'scissors') {
+        console.log(`${userChoice} beats ${computerChoice}. Player wins!`);
+        userScore += 1;
+    } else if (userChoice === 'paper' && computerChoice === 'rock') {
+        console.log(`${userChoice} beats ${computerChoice}. Player wins!`);
+        userScore += 1;
+    } else if (userChoice === 'scissors' && computerChoice === 'paper') {
+        console.log(`${userChoice} beats ${computerChoice}. Player wins!`);
+        userScore += 1;
+    } else {
+        console.log(`${computerChoice} beats ${userChoice}. Computer wins!`);
+        computerScore += 1;
+    }
+}
+
+let userChoice = getUserChoice();
+let computerChoice = getComputerChoice();
+
+playRound(computerChoice, userChoice);
